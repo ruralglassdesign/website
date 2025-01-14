8d098d4ab698c7bba93e01aa2f9885a125c15fd9
Brittany Ellich <brittanyellich@github.com>
2025-01-13 18:50:58 -0800

    Update color scheme to pink

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,25 @@
 import { defineCollection, z } from 'astro:content';
 
+const linkHashtags = (caption: string) => {
+    return caption.replace(/#[a-zA-Z0-9]*/g, (match) => `<a href="https://www.instagram.com/explore/tags/${match.substring(1)}" target="_blank" rel="noopener noreferrer" class="App-link"><strong>${match}</strong></a>`);
+};
+
+const linkAccountNames = (caption: string) => {
+    return caption.replace(/@[a-zA-Z0-9.]*/g, (match) => `<a href="https://www.instagram.com/${match.substring(1)}" target="_blank" rel="noopener noreferrer" class="App-link"><strong>${match}</strong></a>`);
+};
+
+const linkLinks = (caption: string) => {
+    return caption.replace(/(https?:\/\/[^\s]+)/g, (match) => `<a href="${match}" target="_blank" rel="noopener noreferrer" class="App-link">${match}</a>`);
+};
+
+const createCaptions = (caption: string) => {
+    const linkedLinks = linkLinks(caption);
+    const linkedHashtags = linkHashtags(linkedLinks);
+    const linkedAccountNames = linkAccountNames(linkedHashtags);
+    
+    return linkedAccountNames;
+}
+
 export const collections = {
 	work: defineCollection({
 		// Load Instagram posts from the REST API
@@ -7,8 +27,11 @@ export const collections = {
 			const response = await fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,permalink&access_token=${import.meta.env.VITE_ACCESS_TOKEN}`);
 			const data = await response.json();
 			// Must return an array of entries with an id property, or an object with IDs as keys and entries as values
-			return data.data.filter((post: { media_type: string; }) => post.media_type !== 'VIDEO')
-		  },
+			return data.data.filter((post: { media_type: string; }) => post.media_type !== 'VIDEO').map((post: { caption: string }) => ({
+				...post,
+				caption: createCaptions(post.caption)
+			}));
+		},
 		schema: z.object({
 			id: z.string(),
 			caption: z.string(),
