4888c3ac09302deaa83a5c526c29bdaa0d27002b
Brittany Ellich <brittanyellich@github.com>
2025-01-13 16:26:16 -0800

    first commit

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,17 +1,20 @@
-import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 export const collections = {
 	work: defineCollection({
-		// Load Markdown files in the src/content/work directory.
-		loader: glob({ base: './src/content/work', pattern: '**/*.md', }),
+		// Load Instagram posts from the REST API
+		loader: async () => {
+			const response = await fetch(`https://graph.instagram.com/me/media?fields=id,caption,media_type,media_url,permalink&access_token=${import.meta.env.VITE_ACCESS_TOKEN}`);
+			const data = await response.json();
+			// Must return an array of entries with an id property, or an object with IDs as keys and entries as values
+			return data.data.filter((post: { media_type: string; }) => post.media_type !== 'VIDEO')
+		  },
 		schema: z.object({
-			title: z.string(),
-			description: z.string(),
-			publishDate: z.coerce.date(),
-			tags: z.array(z.string()),
-			img: z.string(),
-			img_alt: z.string().optional(),
+			id: z.string(),
+			caption: z.string(),
+			media_type: z.string(),
+			media_url: z.string(),
+			permalink: z.string(),
 		}),
 	}),
 };
